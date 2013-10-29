83fe7702f48807891d1c9ba543c8e700ac8eef46
Natalia Stanko <anithaly@gmail.com>
2013-10-29 15:30:44 +0100

    Program participants redirect to program list

--- src/Impact/ProgramBundle/Controller/ProgramController.php
+++ src/Impact/ProgramBundle/Controller/ProgramController.php
@@ -192,7 +192,7 @@ class ProgramController extends Controller
 
         if ($editForm->isValid()) {
             $em->flush();
-            return $this->redirect($this->generateUrl('program_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('program'));
         }
 
 
